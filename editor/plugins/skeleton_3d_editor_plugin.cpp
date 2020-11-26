03fe63fc07ed4019f7cb4f1de4033e54561cf5d2
Addmix <addmixtaylor@gmail.com>
2020-11-25 17:49:33 -0700

    Revert "Update skeleton_3d_editor_plugin.cpp"
    This reverts commit a82f4a1cd0b2a9e5d0b5ad1520601ea2222a57d7.

--- editor/plugins/skeleton_3d_editor_plugin.cpp
+++ editor/plugins/skeleton_3d_editor_plugin.cpp
@@ -387,10 +387,6 @@ PhysicalBone3D *Skeleton3DEditor::create_physical_bone(int bone_id, int bone_chi
 	CollisionShape3D *bone_shape = memnew(CollisionShape3D);
 	bone_shape->set_shape(bone_shape_capsule);
 
-	Transform capsule_offset;
-	capsule_offset.basis = Basis(Vector3(1, 0, 0), Vector3(0, 0, 1), Vector3(0, -1, 0));
-	bone_shape->set_transform(capsule_offset);
-
 	Transform body_transform;
 	body_transform.set_look_at(Vector3(0, 0, 0), child_rest.origin, Vector3(0, 1, 0));
 	body_transform.origin = body_transform.basis.xform(Vector3(0, 0, -half_height));
